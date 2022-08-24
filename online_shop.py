323f9bf9a91636b5c68eb96c46d1840674d37758
Eddie_Lin <opda0887@gmail.com>
2022-08-24 18:06:00 +0800

    online_shop_v1.1.1.py
    refined

--- online_shop.py
+++ online_shop.py
@@ -3,6 +3,11 @@ from selenium import webdriver
 import pyautogui
 # edge 的 webdriver 要依照版本來做更新！！
 
+def OpenDriver():
+    driver = webdriver.Edge(executable_path="X:\\edge_driver\\msedgedriver.exe")
+    driver.maximize_window
+    return driver
+
 # 讓使用者輸入他要查的東西(目前所使用的為"蝦皮")
 print("----------")
 want = input("欲查詢的商品：")
@@ -22,64 +27,41 @@ if (want != ""):
 
     if (store == "1") or (store == "1."):
         if money_range == "否":
-            driver = webdriver.Edge(executable_path="X:\\edge_driver\\msedgedriver.exe")
-            driver.maximize_window
-            driver.get(f"https://shopee.tw/search?keyword={want}")
+            OpenDriver().get(f"https://shopee.tw/search?keyword={want}")
         elif money_range == "是":
-            driver = webdriver.Edge(executable_path="X:\\edge_driver\\msedgedriver.exe")
-            driver.maximize_window
-            driver.get(f"https://shopee.tw/search?keyword={want}&maxPrice={maxprice}&minPrice={miniprice}")
+            OpenDriver().get(f"https://shopee.tw/search?keyword={want}&maxPrice={maxprice}&minPrice={miniprice}")
         else :
             print("所請求的方式不對，但仍幫你查找商品")
-            driver = webdriver.Edge(executable_path="X:\\edge_driver\\msedgedriver.exe")
-            driver.maximize_window
-            driver.get(f"https://shopee.tw/search?keyword={want}")
+            OpenDriver().get(f"https://shopee.tw/search?keyword={want}")
         print("已成功協助搜尋")
     elif (store == "2") or (store == "2."):
         if money_range == "否":
-            driver = webdriver.Edge(executable_path="X:\\edge_driver\\msedgedriver.exe")
-            driver.maximize_window
-            driver.get(f"https://www.momoshop.com.tw/search/searchShop.jsp?keyword={want}")
+            OpenDriver().get(f"https://www.momoshop.com.tw/search/searchShop.jsp?keyword={want}")
         elif money_range == "是":
-            driver = webdriver.Edge(executable_path="X:\\edge_driver\\msedgedriver.exe")
-            driver.maximize_window
-            driver.get(f"https://www.momoshop.com.tw/search/searchShop.jsp?keyword={want}&_advPriceS={miniprice}&_advPriceE={maxprice}")
+            OpenDriver().get(f"https://www.momoshop.com.tw/search/searchShop.jsp?keyword={want}&_advPriceS={miniprice}&_advPriceE={maxprice}")
         else :
             print("所請求的方式不對，但仍幫你查找商品")
-            driver = webdriver.Edge(executable_path="X:\\edge_driver\\msedgedriver.exe")
-            driver.maximize_window
-            driver.get(f"https://www.momoshop.com.tw/search/searchShop.jsp?keyword={want}")
+            OpenDriver().get(f"https://www.momoshop.com.tw/search/searchShop.jsp?keyword={want}")
         print("已成功協助搜尋")
     elif (store == "3") or (store == "3."):
         if money_range == "否":
-            driver = webdriver.Edge(executable_path="X:\\edge_driver\\msedgedriver.exe")
-            driver.maximize_window
-            driver.get(f"https://www.ruten.com.tw/find/?q={want}")
+            OpenDriver().get(f"https://www.ruten.com.tw/find/?q={want}")
         elif money_range == "是":
-            driver = webdriver.Edge(executable_path="X:\\edge_driver\\msedgedriver.exe")
-            driver.maximize_window
-            driver.get(f"https://www.ruten.com.tw/find/?q={want}&prc.now={miniprice}-{maxprice}")
+            OpenDriver().get(f"https://www.ruten.com.tw/find/?q={want}&prc.now={miniprice}-{maxprice}")
         else :
             print("所請求的方式不對，但仍幫你查找商品")
-            driver = webdriver.Edge(executable_path="X:\\edge_driver\\msedgedriver.exe")
-            driver.maximize_window
-            driver.get(f"https://www.ruten.com.tw/find/?q={want}")
+            OpenDriver().get(f"https://www.ruten.com.tw/find/?q={want}")
         print("已成功協助搜尋")
     elif (store == "4") or (store == "4."):
         if (money_range == "否") or (money_range == "否"):
-            driver = webdriver.Edge(executable_path="X:\\edge_driver\\msedgedriver.exe")
-            driver.maximize_window
-            driver.get(f"https://www.amazon.com/s?k={want}")
+            OpenDriver().get(f"https://www.amazon.com/s?k={want}")
         else:
             print("所請求的方式不對，但仍幫你查找商品")
-            driver = webdriver.Edge(executable_path="X:\\edge_driver\\msedgedriver.exe")
-            driver.maximize_window
-            driver.get(f"https://www.amazon.com/s?k={want}")
+            OpenDriver().get(f"https://www.amazon.com/s?k={want}")
     elif (store == "5") or (store == "5."):
         page = 1
+        driver = OpenDriver()
         if money_range == "否":
-            driver = webdriver.Edge(executable_path="X:\\edge_driver\\msedgedriver.exe")
-            driver.maximize_window
             driver.get(f"https://shopee.tw/search?keyword={want}")
 
             #[1]：按下 Ctrl + T 來新增分頁，而且要搭配pyautogui
@@ -98,8 +80,6 @@ if (want != ""):
             driver.switch_to.window(driver.window_handles[page])
             driver.get(f"https://www.amazon.com/s?k={want}")
         elif money_range == "是":
-            driver = webdriver.Edge(executable_path="X:\\edge_driver\\msedgedriver.exe")
-            driver.maximize_window
             driver.get(f"https://shopee.tw/search?keyword={want}&maxPrice={maxprice}&minPrice={miniprice}")
             pyautogui.hotkey('ctrl', 't', interval=0.1)
             driver.switch_to.window(driver.window_handles[page])
