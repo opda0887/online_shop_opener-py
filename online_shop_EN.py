5e3892f5b4aad87555b0c72b057b3385904424ac
Kiwi_Lin <opda0887@gmail.com>
2022-09-30 16:39:29 +0800

    upd: debug and add EN version

--- online_shop_EN.py
+++ online_shop_EN.py
@@ -1,86 +1,100 @@
-# 商品查詢器~
+# Products searcher~
 from selenium import webdriver
 import pyautogui
-# edge 的 webdriver 要依照版本來做更新！！
+# edge's webdriver need to update with version！！
 
 def OpenDriver():
-    # executable_path 因人而異，請注意!!
+    # executable_path varies from person to person!!
     driver = webdriver.Edge(executable_path="X:\\edge_driver\\msedgedriver.exe")
     driver.maximize_window
     return driver
 
+def Error(type):
+    if (type == "Input"):
+        print("Input Error, but still help tou to find products.")
+    elif (type == "Shop"):
+        print("There is currently no such online shop to search, or your input is wrong~")
+    else:
+        print("! Error !")
+
+
 # ----------------------------------------------------------
 #                        Main program
 # ----------------------------------------------------------
-# 讓使用者輸入他要查的東西
+# Let user input what they want.
 print("----------")
-want = input("欲查詢的商品：")
+want = input("Want to find... ：")
 
 if (want.replace(" ", "") != ""):
 
-    money_range = input("是否要開啟價格範圍(填入\"是\" or \"否\")：")
-    if money_range == "是":
-        miniprice = input("所需的最<低>價位：")
-        maxprice = input("所需的最<高>價位：")
-    elif money_range == "否":
-        #　不會做任何事
+    money_range = input("Whether to open the price range(input \"y\" or \"n\" )：")
+    checkYsame = money_range == "y" or money_range == "Y"
+    checkNsame = money_range == "n" or money_range == "N"
+    checkYnt = money_range != "y" and money_range != "Y"
+    checkNnt = money_range != "n" and money_range != "N"
+
+    if checkYsame:
+        miniprice = input("The lowest price you want... ：")
+        maxprice = input("The highest price you want... ：")
+    elif checkNsame:
+        #　Won't do anything.
         skip = 1 
     else :
-        print("所輸入的方式有誤-視為 \"否\" 來查找商品")
+        print("Input Error - seems \"n\" to find products.")
 
-    store = input("欲在哪裡搜尋{ 1.蝦皮、2.momo、3.露天、4.Amazon(無價格範圍)、5.以上四者皆要 } --- 輸入編號：")
+    store = input("Want to search in... { 1.shopee、2.momo、3.ruten、4.Amazon(no price range)、5.Above of all } --- input serial number：")
 
     if (store == "1") or (store == "1."):
         driver = OpenDriver()
-        if money_range == "否" or money_range != "是":
-            if (money_range != "是" and money_range != "否"):
-                print("所請求的方式不對，但仍幫你查找商品")
+        if checkNsame or checkYnt:
+            if (checkYnt and checkNnt):
+                Error("Input")
             driver.get(f"https://shopee.tw/search?keyword={want}")
         else:
             driver.get(f"https://shopee.tw/search?keyword={want}&maxPrice={maxprice}&minPrice={miniprice}")
-        print("已成功協助搜尋")
+        print("Succeed")
 
     elif (store == "2") or (store == "2."):
         driver = OpenDriver()
-        if money_range == "否" or money_range != "是":
-            if (money_range != "是" and money_range != "否"):
-                print("所請求的方式不對，但仍幫你查找商品")
+        if checkNsame or checkYnt:
+            if (checkYnt and checkNnt):
+                Error("Input")
             driver.get(f"https://www.momoshop.com.tw/search/searchShop.jsp?keyword={want}")
         else:
             driver.get(f"https://www.momoshop.com.tw/search/searchShop.jsp?keyword={want}&_advPriceS={miniprice}&_advPriceE={maxprice}")
-        print("已成功協助搜尋")
+        print("Succeed")
 
     elif (store == "3") or (store == "3."):
         driver = OpenDriver()
-        if money_range == "否" or money_range != "是":
-            if (money_range != "是" and money_range != "否"):
-                print("所請求的方式不對，但仍幫你查找商品")
+        if checkNsame or checkYnt:
+            if (checkYnt and checkNnt):
+                Error("Input")
             driver.get(f"https://www.ruten.com.tw/find/?q={want}")
         else:
             driver.get(f"https://www.ruten.com.tw/find/?q={want}&prc.now={miniprice}-{maxprice}")
-        print("已成功協助搜尋")
+        print("Succeed")
 
     elif (store == "4") or (store == "4."):
         driver = OpenDriver()
-        if money_range == "是":
-            print("目前沒有價格查詢功能，但仍幫你查找商品")
-        elif money_range != "否":
-            print("所請求的方式不對，但仍幫你查找商品")
+        if checkYsame:
+            print("There is currently no price query function, but it still helps you find products.")
+        elif checkNnt:
+            Error("Input")
         driver.get(f"https://www.amazon.com/s?k={want}")
-        print("已成功協助搜尋")
+        print("Succeed")
 
     elif (store == "5") or (store == "5."):
         page = 1
         driver = OpenDriver()
         
-        if money_range == "否":
+        if checkNsame:
             driver.get(f"https://shopee.tw/search?keyword={want}")
 
-            #[1]：按下 Ctrl + T 來新增分頁，而且要搭配pyautogui
+            #[1]：click Ctrl + T to add new page
             pyautogui.hotkey('ctrl', 't', interval=0.1) #[1]
-            #[2]：切換瀏覽器的window handle
+            #[2]：switch webdriver's window handle
             driver.switch_to.window(driver.window_handles[page]) #[2]
-            #[3]：page記得+1
+            #[3]：remind that: page+1
             page+=1 #[3]
             driver.get(f"https://www.momoshop.com.tw/search/searchShop.jsp?keyword={want}")
 
@@ -93,7 +107,8 @@ if (want.replace(" ", "") != ""):
             driver.switch_to.window(driver.window_handles[page])
             page+=1
             driver.get(f"https://www.amazon.com/s?k={want}")
-        elif money_range == "是":
+
+        elif checkYsame:
             driver.get(f"https://shopee.tw/search?keyword={want}&maxPrice={maxprice}&minPrice={miniprice}")
             
             pyautogui.hotkey('ctrl', 't', interval=0.1)
@@ -110,11 +125,11 @@ if (want.replace(" ", "") != ""):
             driver.switch_to.window(driver.window_handles[page])
             page+=1
             driver.get(f"https://www.amazon.com/s?k={want}")
-        print("已成功協助搜尋")
+        print("Succeed")
     else :
-        print("目前無提供此類商城來搜尋，或者是您的輸入有誤~")
+        Error("Shop")
 
 elif (want.replace(" ", "") == ""):
-    print("! 您的輸入方式有誤 !")
+    Error("")
 
 print("----------")
